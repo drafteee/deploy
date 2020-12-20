816581247b689b4a7f080d1ccfa0910e95ab2084
drafteee <sergej_-96@mail.ru>
2020-12-20 21:05:39 +0300

    up

---
+++ server.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, redirect
+import csv
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+  return render_template(page_name)
+
+def write_to_file(data):
+  with open('database.csv', newline='', mode='a') as database:
+    email = data['email']
+    subject = data['subject']
+    message = data['message']
+    csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    csv_writer.writerow([email, subject, message])
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+  if request.method == 'POST':
+    try:
+      data = request.form.to_dict()
+      print(data)
+      write_to_file(data)
+      return redirect('/thankyou.html')
+    except:
+      return 'save wrong'
+  else:
+    'wrong'
+
+# @app.route('/works.html')
+# def works():
+#   return render_template('works.html')
+
+# @app.route('/about.html')
+# def about():
+#   return render_template('about.html')
+
+# @app.route('/contact.html')
+# def contact():
+#   return render_template('contact.html')
+
+# @app.route('/components.html')
+# def components():
+#   return render_template('components.html')
